88e918e8ae9cc30698aba589a0004cfcb8e174da
Lisa Smeke <lisasmeke@gmail.com>
2022-08-30 21:28:57 +0200

    update header, intro text, and link in a tag

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -5,17 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hello!</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>My name is Lisa.</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/lisasmeke/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Connect with me on LinkedIn
         </a>
       </header>
     </div>
